91ba3370e550b883f992b6c9554017becce93527
Fry, Logan <logan.fry@rovisys.com>
2018-03-22 18:04:31 -0400

    Created extended forecast file

---
+++ src/components/ExtendedForecast.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var api = require('../util/Api');
+
+class ExtendedForecast extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        
+    }
+}
+
+module.exports = ExtendedForecast;
