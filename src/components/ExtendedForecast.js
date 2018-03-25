dbf5865ca432bf434cb89e04e98f4d8459a87396
logan-fry <loganfrybball@yahoo.com>
2018-03-24 20:39:38 -0400

    Got openweather api call to return data

--- src/components/ExtendedForecast.js
+++ src/components/ExtendedForecast.js
@@ -1,16 +1,36 @@
 var React = require('react');
+var QueryString = require('query-string');
+var api = require('../util/api');
 
 class ExtendedForecast extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-
+            response: null,
         }
     }
 
+    componentDidMount(){
+        var params = QueryString.parse(this.props.location.search);
+        var city = params.city;
+        var country = params.country;
+        api.Get5DayForecast(city, country)
+            .then(function(data){
+                return this.setState({
+                    response: data,
+                })
+            }.bind(this))
+    }
+
     render() {
-        return <p>Hello from extended forecast</p>;
+        return (
+            <div className='container'>
+                {this.state.response ? 
+                    <p>{JSON.stringify(this.state.response)}</p> 
+                    : <p>Loading</p>}
+            </div>
+        )
     }
 }
 
--- src/util/api.js
+++ src/util/api.js
@@ -1,15 +1,15 @@
 var axios = require('axios');
-var apiKey = '811793717d22527c63d02e165dda39c5';
-var baseUrl = 'api.openweathermap.org/data/2.5/forecast'
 
 function Get5DayForecast(city, country){
-    axios.get(baseUrl + '?q=' + city + ',' + country + '&APPID=' + apiKey)
+    var apiKey = '811793717d22527c63d02e165dda39c5';
+    var url = 'https://api.openweathermap.org/data/2.5/forecast';
+    var encodedUrl = window.encodeURI(url + '?q=' + city + ',' + country + '&APPID=' + apiKey);
+
+    return axios.get(encodedUrl)
         .then(function(response){
-            console.log(response);
+            console.log(response.data)
+            return response.data;
         })
-        .catch(function(error){
-            console.log(error);
-        });
 }
 
 module.exports = {
