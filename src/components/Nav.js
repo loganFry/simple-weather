bf42fa60e12e7ae0e02d49bed8d52d3d3fe13793
logan-fry <loganfrybball@yahoo.com>
2018-03-26 21:23:35 -0400

    Made app name into link to home page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,12 @@
 var React = require("react");
 var WeatherForm = require('./WeatherForm');
+var Link = require('react-router-dom').Link;
 
 class Nav extends React.Component {
   render() {
       return (
         <div className='nav'>
-            <h1>Simple Weather</h1>
+            <Link to='/' className='header'>Simple Weather</Link>
             <WeatherForm direction='row'/>
         </div>
       );
