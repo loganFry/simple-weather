051b27120eae7a2f6e67f4fc9883479ceaa589a5
Logan Fry <loganfrybball@yahoo.com>
2018-04-04 20:24:00 -0400

    Added media query to stack menu on small screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
 var React = require("react");
 var WeatherForm = require('./WeatherForm');
 var Link = require('react-router-dom').Link;
+var MediaQuery = require('react-responsive').default;
 
 class Nav extends React.Component {
   render() {
       return (
-        <div className='nav'>
-            <Link to='/' className='header'>Simple Weather</Link>
-            <WeatherForm direction='row'/>
+        <div>
+            <MediaQuery minWidth={675}>
+              <div className='nav nav-horizontal'>
+                <Link to='/' className='header'>Simple Weather</Link>
+                <WeatherForm direction='row'/>
+              </div>
+            </MediaQuery>
+            <MediaQuery maxWidth={674}>
+              <div className='nav nav-vertical'>
+                <Link to='/' className='header'>Simple Weather</Link>
+                <WeatherForm direction='column'/>
+              </div>
+            </MediaQuery>
         </div>
       );
   }
