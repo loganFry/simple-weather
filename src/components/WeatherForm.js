2b14c16c035de2f88e0554a080b19c27f948befa
Logan Fry <loganfrybball@yahoo.com>
2018-04-08 14:42:08 -0400

    Modified weather form to redirect to forecast component when enter is pressed

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,4 +1,5 @@
 var Link = require('react-router-dom').Link;
+var Redirect = require('react-router-dom').Redirect;
 var React = require('react');
 var PropTypes = require('prop-types');
 
@@ -9,10 +10,32 @@ class WeatherForm extends React.Component {
     this.state = {
       text: "",
       city: "",
-      country: ""
+      country: "",
+      redirect: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  componentDidUpdate(){
+    // after a redirect completes, reset the redirect property on
+    // the state
+    if(this.state.redirect === true){
+      this.setState({
+        redirect: false,
+      })
+    }
+  }
+
+  handleKeyUp(event){
+    // if the user presses Enter, change the state to render a redirect
+    // to the forecast page
+    if(event.key === 'Enter'){     
+      this.setState({
+        redirect: true,
+      })
+    }
   }
 
   handleChange(event){
@@ -38,19 +61,25 @@ class WeatherForm extends React.Component {
 
   render(){
     return (
-      <div style={{flexDirection: this.props.direction}} className='weather-container'>
+      this.state.redirect === false ? 
+      <div 
+      style={{flexDirection: this.props.direction}} 
+      className='weather-container'
+      onKeyUp={this.handleKeyUp}>
         <input 
           type='text' 
           className='form-control'
           value={this.state.text} 
           onChange={this.handleChange}
-          placeholder='Ex. Los Angeles, USA'/>
+          placeholder='Ex. Los Angeles, USA'
+        />
         <Link 
           to={'/forecast?city=' + this.state.city + '&country=' + this.state.country}
           className='button button-success'>
           Get Weather
         </Link>
       </div>
+      : <Redirect to={'/forecast?city=' + this.state.city + '&country=' + this.state.country} />
     )
   }
 }
